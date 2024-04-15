f0d7dd5ceaefb9e5669c369e2142088bc9744efd
NYBotx <167092531+NYBotx@users.noreply.github.com>
2024-04-15 18:59:53 +0530

    Update config.py

--- ocrbot/config.py
+++ ocrbot/config.py
@@ -3,5 +3,5 @@ import dotenv
 
 dotenv.load_dotenv()
 
-API_KEY = os.environ.get("API_KEY") # api key from https://ocr.space/ocrapi
-BOT_TOKEN = os.environ.get("BOT_TOKEN") # bot token from @BotFather
+API_KEY = os.environ.get("K86351489588957) # api key from https://ocr.space/ocrapi
+BOT_TOKEN = os.environ.get("7169625453:AAG7m4N48VVbBCSfAV7tR5nMglJytYS-JWQ") # bot token from @BotFather
